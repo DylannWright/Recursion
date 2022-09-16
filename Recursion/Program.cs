a5b690837c2367cdfec36904ec67eba6a1cef63a
Dylan Wright <17WrighD@fulford.york.sch.uk>
2022-09-16 14:49:01 +0100

    Add project files.

---
+++ Recursion/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            input 
+            int number = 
+
+        }
+
+
+        /// <summary>
+        /// Factorial
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+
+        private static string Factorial(int number)
+        {
+            int count = number - 1;
+            number = number * count;
+            
+
+             
+            
+
+            return number;
+
+        }
+    }
+}
